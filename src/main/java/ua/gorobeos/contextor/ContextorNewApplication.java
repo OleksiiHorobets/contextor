21a0bf780c92b468492f5f07f31ccb950590e83e
Alex Horobets <sgs.cat.fly@gmail.com>
2025-05-24 16:51:14 +0300

    init commit

---
+++ src/main/java/ua/gorobeos/contextor/ContextorNewApplication.java
@@ -0,0 +1,13 @@
+package ua.gorobeos.contextor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextorNewApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ContextorNewApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ua/gorobeos/contextor/ContextorNewApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.gorobeos.contextor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextorNewApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
